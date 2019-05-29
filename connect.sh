93f928058e2fb085933aa2a6817dca3c9c3bc341
AK Sarav <sarasoft@live.com>
2019-05-30 02:53:08 +0530

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -28,17 +28,17 @@ do
         #sshpass -p.passwd ssh $LINE "uname -n"
         ssh-keygen -R $LINE
         DIRNAME="/var/tmp/inventory_data_"$DATESTR"/"
-        sshpass -f.passwd ssh   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no sthangar@$LINE "mkdir $DIRNAME && chmod -R 777 $DIRNAME & cd $DIRNAME"
-        sshpass -f.passwd scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no proj.tar sthangar@$LINE:$DIRNAME.
-        sshpass -f.passwd ssh   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no sthangar@$LINE "cd $DIRNAME && tar -xvf proj.tar && sudo -u weblogic ./take_configxml.sh $DIRNAME"
+        sshpass -f.passwd ssh   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no username@$LINE "mkdir $DIRNAME && chmod -R 777 $DIRNAME & cd $DIRNAME"
+        sshpass -f.passwd scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no proj.tar username@$LINE:$DIRNAME.
+        sshpass -f.passwd ssh   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no username@$LINE "cd $DIRNAME && tar -xvf proj.tar && sudo -u weblogic ./take_configxml.sh $DIRNAME"
         if [ ! -e inventory/$LINE ]
         then
                 mkdir inventory/$LINE
         fi
-        sshpass -f.passwd scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r sthangar@$LINE:$DIRNAME/*domain_dumps*  inventory/$LINE/
-        sshpass -f.passwd scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r sthangar@$LINE:$DIRNAME/*.host inventory/$LINE/
+        sshpass -f.passwd scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r username@$LINE:$DIRNAME/*domain_dumps*  inventory/$LINE/
+        sshpass -f.passwd scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r username@$LINE:$DIRNAME/*.host inventory/$LINE/
         echo "REMOVING THE /var/tmp/inventory_data_* DIRECTORIES"
-        sshpass -f.passwd ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no sthangar@$LINE "rm -rf /var/tmp/inventory_data_*"
+        sshpass -f.passwd ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no username@$LINE "rm -rf /var/tmp/inventory_data_*"
         ## Python Script to take care of these files
         echo "Press enter to Proceed to next instance"
         read tmp
